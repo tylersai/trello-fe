82d50d21ad2f3c9fae4596280f7605ef60adaa87
Sai Han <thesaihan@gmail.com>
2020-06-01 00:05:32 +0630

    Set max-height of wrapper

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 window.onload = () => {
   console.log("DOM is ready!");
+  limitWrapperHeight();
 };
+
+function limitWrapperHeight() {
+  const body = document.documentElement.clientHeight;
+  const nav1 = document.getElementById("first-nav").clientHeight;
+  const nav2 = document.getElementById("second-nav").clientHeight;
+  const wrapper = document.getElementById("wrapper");
+  wrapper.style.maxHeight = (body - nav1 - nav2) + "px";
+}
